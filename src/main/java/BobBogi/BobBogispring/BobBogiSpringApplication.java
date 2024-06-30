dbf39f05c8fc215207384b0dc0e01170b87a03f3
Choi-seokun <142799427+Choi-seokun@users.noreply.github.com>
2024-07-01 01:15:59 +0900

    Update BobBogiSpringApplication.java

--- src/main/java/BobBogi/BobBogispring/BobBogiSpringApplication.java
+++ src/main/java/BobBogi/BobBogispring/BobBogiSpringApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class BobBogiSpringApplication {
-
+	int a;
 	public static void main(String[] args) {
 		SpringApplication.run(BobBogiSpringApplication.class, args);
 	}
